2699b4d264de95e2af261e7fc73db0dfdcf37758
Aditya Soni <adityanithariya@gmail.com>
2023-10-14 23:19:34 +0530

    Configtx Added

--- hyperledger/network/cryptogen/init.sh
+++ hyperledger/network/cryptogen/init.sh
@@ -3,6 +3,9 @@ function generateCryptoMaterial() {
 
     : ${CRYPTOGEN_PATH:="organizations"}
 
+    # Remove previous crypto material
+    rm -rf ./${CRYPTOGEN_PATH}
+
     # eVault Orgs
     cryptogen generate --config=./cryptogen/evault/config-india.yaml --output=$CRYPTOGEN_PATH
     cryptogen generate --config=./cryptogen/evault/config-raj.yaml --output=$CRYPTOGEN_PATH
@@ -10,4 +13,23 @@ function generateCryptoMaterial() {
     # Judiciary Orgs
     cryptogen generate --config=./cryptogen/judiciary/config-sci.yaml --output=$CRYPTOGEN_PATH
     cryptogen generate --config=./cryptogen/judiciary/config-hcraj.yaml --output=$CRYPTOGEN_PATH
+
+    cryptogen generate --config=./cryptogen/config-orderer.yaml --output=$CRYPTOGEN_PATH
+}
+
+function generateGenesisBlock() {
+    echo "Generating genesis block"
+
+    cp ./config/configtx.yaml .
+
+    : ${CRYPTOGEN_PATH:="organizations"}
+    export FABRIC_CFG_PATH=.
+
+    configtxgen -profile OrdererGenesis -channelID system-channel -outputBlock ./$CRYPTOGEN_PATH/genesis.block
+
+    configtxgen -profile Raj -outputCreateChannelTx ./$CRYPTOGEN_PATH/raj.tx -channelID raj
+
+    configtxgen -profile HCRaj -outputCreateChannelTx ./$CRYPTOGEN_PATH/hcraj.tx -channelID hcraj
+
+    rm ./configtx.yaml
 }
--- hyperledger/network/network.sh
+++ hyperledger/network/network.sh
@@ -4,4 +4,6 @@ chmod +x ./init.sh
 chmod +x ./cryptogen/init.sh
 . ./cryptogen/init.sh
 
+echo $PWD
 generateCryptoMaterial
+generateGenesisBlock
