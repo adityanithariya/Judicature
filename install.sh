cf2f709c6fe12b11bedd3baf767fa36076fae96f
Aditya Soni <adityanithariya@gmail.com>
2024-04-02 14:11:00 +0530

    Install Config Added

---
+++ install.sh
@@ -0,0 +1,37 @@
+echo "Updating & Upgrading...\n\n"
+# sudo apt update -y && apt upgrade -y
+
+echo "Basic Installation...\n\n"
+sudo apt-get install \
+    git \
+    ca-certificates \
+    curl \
+    gnupg \
+    docker \
+    docker.io \
+    python3-pip -y
+
+echo "Code Install...\n\n"
+sudo snap install code
+
+echo "Docker Install...\n\n"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+echo "nvm Install...\n\n"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+sudo nvm install 18.17.0
+sudo nvm use 18.17.0
+sudo npm i -g docker-compose
+
+sudo apt-get install libltdl-dev -y
+
+echo "Hyperledger Install..."
+# curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
+# ./install-fabric.sh b d
+
+# sudo reboot
